af7dd318f13d8adde8e8bac6b08c9a876036084c
zmy_gbl <ZMY_gbl@qq.com>
2020-02-07 22:17:24 +0800

    实体类及服务曾层

--- gmall-pms/src/test/java/com/zmy/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/zmy/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,26 @@
 package com.zmy.gmall.pms;
 
+import com.zmy.gmall.pms.entity.Product;
+import com.zmy.gmall.pms.service.ProductAttributeService;
+import com.zmy.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+    @Autowired
+    ProductService productService;
+
     @Test
     void contextLoads() {
+        Product byId = productService.getById(1);
+        System.out.println(byId.getName());
     }
 
 }
